0a30a7544ed7b4d548cde9341b56af0f145f1652
Olaribigbe01 <olaribigbebolaji99@gmail.com>
2023-09-18 10:26:37 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include  <lime.h>
+#include  <time.h>
 
 /**
  *  main - Generates random valid password for the
@@ -12,7 +12,7 @@ int main(void)
 
 {
 	char password[84];
-	int index = 0, diff_half1, diff_half2;
+	int index = 0,sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
 
